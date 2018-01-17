c3189adee29066e6e2064a85a9784fb338311267
MINSK\Dzmitry_Lazouski <Dzmitry_Lazouski@epam.com>
2018-01-18 00:13:19 +0300

    Task 1.2 Completed

--- mongodb_part2.js
+++ mongodb_part2.js
@@ -1,5 +1,5 @@
 //-------------------------------------
-// Aggregating Airlines Collection 1.
+// 1.1
 //-------------------------------------
 db.airlines.aggregate(
 	[
@@ -18,8 +18,66 @@ db.airlines.aggregate(
 		}
 	]
 );
-// // Result
+// Result:
 // { "class" : "F", "total" : 140343 }
 // { "class" : "L", "total" : 23123 }
 // { "class" : "P", "total" : 5683 }
 // { "class" : "G", "total" : 17499 }
+
+
+
+//-------------------------------------
+//  1.2
+//-------------------------------------
+db.airlines.aggregate(
+	[
+		{
+			$match: {
+				"originCountry" : { $eq : "United States"},
+				"destCountry" : { $ne : "United States"}
+			}
+		},
+		{
+			$project: {
+			    _id: 0,
+			    destCity: 1,
+			    passengers: 1
+			}
+		},
+		{
+			$group: {
+				_id: { destCity: "$destCity"},
+				avgPassengers: {$avg: "$passengers"}
+			}
+		},
+		{
+			$sort: {
+				"avgPassengers": -1
+			}
+		},
+		{
+			$limit: 3
+		},
+		{
+			$project: {
+				_id : 0,
+				avgPassengers: "$avgPassengers",
+				city: "$_id.destCity"
+			}
+		}
+	]
+);
+// Result:
+// { 
+//     "avgPassengers" : 8052.380952380952, 
+//     "city" : "Abu Dhabi, United Arab Emirates"
+// }
+// { 
+//     "avgPassengers" : 7176.596638655462, 
+//     "city" : "Dubai, United Arab Emirates"
+// }
+// { 
+//     "avgPassengers" : 7103.333333333333, 
+//     "city" : "Guangzhou, China"
+// }
+
